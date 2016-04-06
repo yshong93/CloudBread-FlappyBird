0e1be660124a6f82882ca50cf07075864f89574f
YoonSeok Hong <yshong93@outlook.com>
2016-04-06 13:59:27 +0900

    get facebook access token

--- Assets/Scripts/FacbookLogin.cs
+++ Assets/Scripts/FacbookLogin.cs
@@ -1,11 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Facebook.Unity;
 
 public class FacbookLogin : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    // Awake function from Unity's MonoBehavior
+    void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            // Initialize the Facebook SDK
+            FB.Init(InitCallback, OnHideUnity);
+        }
+        else {
+            // Already initialized, signal an app activation App Event
+            FB.ActivateApp();
+        }
+    }
+
+    private void InitCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        }
+        else {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+    private void LoginwithPermissions()
+    {
+        var perms = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(perms, AuthCallback);
+    }
+
+    private void AuthCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            // AccessToken class will have session details
+            var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+            // Print current access token's User ID
+            Debug.Log(aToken.UserId);
+            // Print current access token's granted permissions
+            foreach (string perm in aToken.Permissions)
+            {
+                Debug.Log(perm);
+            }
+
+            StartGame();
+
+            PlayerPrefs.DeleteAll();
+            //PlayerPrefs.SetString("access_token", aToken);
+        }
+        else {
+            Debug.Log("User cancelled login");
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
@@ -13,9 +74,30 @@ public class FacbookLogin : MonoBehaviour {
 	
 	}
 
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+
+    private void StartGame()
+    {
+        //PlayerPrefs.SetString("access_token", "0000");
+        SceneManager.LoadScene("mainGame");
+    }
+
     public void LoginButtonClicked()
     {
+        if (!FB.IsLoggedIn)
+            LoginwithPermissions();
 
-
+        print("LoginButtonClicked");
     }
 }
