bd152d2306cdc920c4191937dd2417741a5b010f
YoonSeok Hong <yshong93@outlook.com>
2016-04-11 18:16:11 +0900

    Utils about encoding, decoding

---
+++ Assets/Scripts/CloudBread/CBUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Assets.Scripts.CloudBread
+{
+    class CBUtils
+    {
+        public static string CBEncoding(string str)
+        {
+            // utf-8 인코딩
+            byte[] bytesForEncoding = Encoding.UTF8.GetBytes(str);
+            string encodedString = Convert.ToBase64String(bytesForEncoding);
+            
+
+            // utf-8 디코딩
+            byte[] decodedBytes = Convert.FromBase64String(encodedString);
+            string decodedString = Encoding.UTF8.GetString(decodedBytes);
+
+            return decodedString;
+        }
+    }
+}
