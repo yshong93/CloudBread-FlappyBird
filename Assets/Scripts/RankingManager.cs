43e8ffb06dd3ffe4dcbc722c34711159e8b0fba3
YoonSeok Hong <yshong93@outlook.com>
2016-04-11 18:15:17 +0900

    add Ranking Manage to use CloudBread Ranking api

---
+++ Assets/Scripts/RankingManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RankingManager : MonoBehaviour {
+
+    public Text RankingText;
+    public string RankingString;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
