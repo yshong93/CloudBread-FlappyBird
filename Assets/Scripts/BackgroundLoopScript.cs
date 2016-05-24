894449900ffb5d610eb5c12a8fe3ab46e451fbe4
HONGYOONSEOK <yoonseok417@gmail.com>
2016-05-24 13:14:41 +0900

    make background moving

---
+++ Assets/Scripts/BackgroundLoopScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundLoopScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.localPosition.x < -2.58f)
+		{
+			transform.localPosition = new Vector3(0, transform.localPosition.y, transform.localPosition.z);
+		}
+		transform.Translate(-Time.deltaTime, 0, 0);
+	}
+}
